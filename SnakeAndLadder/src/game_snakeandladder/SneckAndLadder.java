b472a63e407c0b3368b005ccaabd1c1519b54700
Kajal <kajalpatil158@gmail.com>
2021-03-23 11:44:00 +0530

    [Kajal Patil]Added: Condition For Snake Ladder And No_Play

--- SnakeAndLadder/src/game_snakeandladder/SneckAndLadder.java
+++ SnakeAndLadder/src/game_snakeandladder/SneckAndLadder.java
@@ -3,8 +3,18 @@ package game_snakeandladder;
 public class SneckAndLadder {
 
 	public static void main(String[] args) {
-		final int Play = 1;
+		final int NO_PLAY = 0;
+		final int LADDER = 1;
+		final int SNAKE = 2;
 		int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
 		System.out.println("Player Get Number = " + die);
+		int checkCondition = (int) Math.floor(Math.random() * 10) % 3;
+		if (checkCondition == NO_PLAY) {
+			System.out.println("Player No Play");
+		} else if (checkCondition == LADDER) {
+			System.out.println("Its Ladder");
+		} else {
+			System.out.println("Its Snake");
+		}
 	}
 }
