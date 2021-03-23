a6237961d8e4e8e7bdcd490337059100e4cf8aca
Kajal <kajalpatil158@gmail.com>
2021-03-23 11:25:24 +0530

    [Kajal Patil]Added: SnakeAndLadder Game Start With Initial Position

---
+++ SnakeAndLadder/src/game_snakeandladder/SneckAndLadder.java
@@ -0,0 +1,10 @@
+package game_snakeandladder;
+
+public class SneckAndLadder {
+
+	public static void main(String[] args) {
+		final int Play = 1;
+		System.out.println("Game Start With Position Zero:");
+	}
+
+}
